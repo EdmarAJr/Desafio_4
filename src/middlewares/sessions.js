da06efcf4c1a46d4b3d38e9e911b25ea5175b3b7
EdmarAJr <trovador@hotmail.com.br>
2020-11-24 23:08:09 -0300

    FEAT: Middleware de sessão

---
+++ src/middlewares/sessions.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+
+const jwt = require('jsonwebtoken');
+const response = require('../controllers/response');
+
+require('dotenv').config();
+
+const verify = async (ctx, next) => {
+	const { authorization = null } = ctx.headers;
+	if (authorization) {
+		const [bearer, token] = authorization.split(' ');
+		if (token !== 'undefined') {
+			try {
+				const verification = await jwt.verify(
+					token,
+					process.env.JWT_SECRET
+				);
+
+				ctx.state.userId = verification.id;
+				ctx.state.email = verification.email;
+			} catch (err) {
+				console.log(err);
+				return response(ctx, 403, 'Ação proibida session');
+			}
+
+			return next();
+		}
+	}
+
+	return response(ctx, 403, 'Ação proibida session 2');
+};
+
+module.exports = { verify };
