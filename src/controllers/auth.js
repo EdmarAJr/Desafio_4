b2103f5b6eddf2d615bcb1984c51cf711e7ac45b
EdmarAJr <trovador@hotmail.com.br>
2020-11-28 15:17:10 -0300

    REFACTOR: alterações nos nomes da variável password >> senha.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,20 +1,22 @@
 const jwt = require('jsonwebtoken');
 const response = require('./response');
 const Usuarios = require('../repositories/usuariosBancoDeDados');
-const Password = require('../utils/password');
+const Senha = require('../utils/password');
 
 require('dotenv').config();
 
 const autenticar = async (ctx) => {
-	const { email = null, password = null } = ctx.request.body;
-	if (!email || !password) {
-		return response(ctx, 400, { mensagem: 'Pedido mal formatado auth' });
+	const { email = null, senha = null } = ctx.request.body;
+	if (!email || !senha) {
+		return response(ctx, 400, {
+			mensagem: 'Pedido de autenticação mal formatado',
+		});
 	}
 
 	const usuario = await Usuarios.obterUsuarioPorEmail(email);
 
 	if (usuario) {
-		const comparison = await Password.check(password, usuario.senha);
+		const comparison = await Senha.check(senha, usuario.senha);
 		if (comparison) {
 			const token = await jwt.sign(
 				{ id: usuario.id, email: usuario.email },
