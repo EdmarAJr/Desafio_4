71ac5ff1f3659353f1e642fb2f16b2dcdd65f2f3
EdmarAJr <trovador@hotmail.com.br>
2020-12-03 15:30:53 -0300

    FIX: Correções no parserInt e remoçao do asinc

--- src/utils/testarCpf.js
+++ src/utils/testarCpf.js
@@ -1,28 +1,25 @@
+/* eslint-disable eqeqeq */
 const testarCPF = (strCPF) => {
 	let soma = 0;
 	let resto = 0;
 
-	// eslint-disable-next-line radix
 	const limparCPF = strCPF.replace(/[^\d]/g, '');
 
 	for (let i = 1; i <= 9; i++) {
 		soma += limparCPF.substring(i - 1, i) * (11 - i);
 		resto = (soma * 10) % 11;
 	}
-	console.log(soma);
 	console.log(1, resto, limparCPF.substring(9, 10));
 	if (resto == 10 || resto == 11) resto = 0;
 	if (resto != limparCPF.substring(9, 10)) return false;
 
 	soma = 0;
 	for (let i = 1; i <= 10; i++) {
-		// eslint-disable-next-line radix
 		soma += limparCPF.substring(i - 1, i) * (12 - i);
 		resto = (soma * 10) % 11;
 	}
-	console.log(2, resto, limparCPF.substring(10, 11));
 	if (resto == 10 || resto == 11) resto = 0;
-	// eslint-disable-next-line radix
+
 	if (resto != limparCPF.substring(10, 11)) return false;
 	return true;
 };
