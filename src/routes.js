ad05772e13be5f783481b27ce4bfbe0f939760bc
EdmarAJr <trovador@hotmail.com.br>
2020-12-04 14:38:56 -0300

    FEAT: adição da rota get cobranca e re relatório.

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,13 @@ const router = new Router();
 const Auth = require('./controllers/auth');
 const Clientes = require('./controllers/clientesController');
 const Usuarios = require('./controllers/usuariosController');
-const Pagamentos = require('./controllers/pagamento');
 const Cobrancas = require('./controllers/cobrancaController');
+const Relatorio = require('./utils/relatorio');
 
 const Password = require('./middlewares/encrypt');
 const Session = require('./middlewares/sessions');
 
 router.post('/auth', Auth.autenticar);
-router.post('/pagamento', Session.verify, Pagamentos.pagamento);
 
 router.post('/usuarios', Password.encrypt, Usuarios.criarUsuario);
 
@@ -21,11 +20,8 @@ router.get('/clientes', Session.verify, Clientes.listarTodosClientes);
 router.put('/clientes', Session.verify, Clientes.editarCliente);
 
 router.post('/cobrancas', Session.verify, Cobrancas.gerarCobranca);
-router.put('/cobranca', Session.verify, Cobrancas.controleDeCobrancas);
+router.get('/cobrancas', Session.verify, Cobrancas.controleDeCobrancas);
 router.put('/cobrancas', Session.verify, Cobrancas.quitarCobranca);
+router.get('/relatorio', Session.verify, Relatorio.formatarRelatorio);
 
 module.exports = router;
-
-
-
-// router.get('/relatorio', Session.verify, Cobranca.obterRelatorio);
